b8a3ab25a651d05d1467c8bbf3a36a477c92e7d9
Gchaurio <17-10126@usb.ve>
2019-11-15 18:44:57 -0400

    Primeras funciones

---
+++ quedanFichas.py
@@ -0,0 +1,14 @@
+def quedanFichas(F:int,R:int)->bool:
+    
+    if F-R==0:
+        quedan=False
+    elif F-R!=0:
+        quedan=True
+    return quedan
+
+"""fichas=60
+moves=5
+quedan= quedanFichas(fichas,moves)"""
+
+
+
