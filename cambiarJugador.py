4b3c7971b2283454a46ee50b66f247fc2e5976ce
Luis Carlos  Blanco <luiscarlosblanco@MacBook-Pro-de-Luis.local>
2019-11-15 19:22:51 -0400

    proc

--- cambiarJugador.py
+++ cambiarJugador.py
@@ -1,9 +1,7 @@
-def cambiarJugador(turn:int) -> "void":
+def cambiarJugador(turn:int) -> 'void':
     if turn==1:
         turn=2
     elif turn==2:
         turn=1
+    return turn
     
-turno=2
-cambiarJugador(turno)
-print(turno)
