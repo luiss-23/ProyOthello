1d46a29d0cc0a28a188386be8b64cd57429b6669
Luis Carlos  Blanco <luiscarlosblanco@MacBook-Pro-de-Luis.local>
2019-11-16 18:37:04 -0400

    squere

--- consumoVertical.py
+++ consumoVertical.py
@@ -1,13 +1,15 @@
 def consumoVertical(A:[int], i:int, j:int, turno:int) -> 'void':
     for i in range(0,8):
         if ( A[i][j] == turno ):
-            if ( A[i+1][j] != 0 and A[i+1][j] != turno ):
+            if ( A[i+1][j] != 0 and A[i+1][j] != turno and 0<=i+1<8):
                 A[i+1][j] = turno
                 consumoVertical(A, i + 1, j, turno)
-            elif ( A[i-1][j] != 0 and A[i-1][j] != turno):
+            elif ( A[i-1][j] != 0 and A[i-1][j] != turno and 0<=i-1<8):
                 A[i-1][j] == turno
                 consumoVertical(A, i - 1, j, turno)
             elif ( A[i+1][j] == 0 or A[i+1][j] == turno ):
                 break
             elif ( A[i-1][j] == 0 or A[i-1][j] == turno):
                 break
+        else:
+            break
