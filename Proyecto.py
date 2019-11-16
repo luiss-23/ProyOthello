830a884ee4e41d426fe79a05c85a89dac3610794
Luis Carlos  Blanco <luiscarlosblanco@MacBook-Pro-de-Luis.local>
2019-11-16 14:03:18 -0400

    Avance

--- Proyecto.py
+++ Proyecto.py
@@ -37,6 +37,20 @@ def cambiarJugador(turn:int) -> 'void':
         turn=1
     return turn
 
+
 def reflejarJugada( A: [int], i: int, j: int, turno: int) -> 'void':
     A[i][j] = turno
 
+def consumoVertical(A:[int], i:int, j:int, turno:int) -> 'void':
+    for i in range(0,8):
+        if ( A[i][j] == turno ):
+            if ( A[i+1][j] != 0 and A[i+1][j] != turno ):
+                A[i+1][j] = turno
+                consumoVertical(A, i + 1, j, turno)
+            elif ( A[i-1][j] != 0 and A[i-1][j] != turno):
+                A[i-1][j] == turno
+                consumoVertical(A, i - 1, j, turno)
+            elif ( A[i+1][j] == 0 or A[i+1][j] == turno ):
+                pass
+            elif ( A[i-1][j] == 0 or A[i-1][j] == turno):
+                pass
