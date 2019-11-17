d08ac207b7909dce9f1fd034f7b6776b5a086b8b
Gchaurio <17-10126@usb.ve>
2019-11-16 21:30:49 -0400

    Fuego fuego waya waya

--- Proyecto.py
+++ Proyecto.py
@@ -5,7 +5,7 @@ import sys
 #Creacion del tablero
 Tablero = [ [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
-            [2, 1, 1, 0, 1, 0, 0, 0],
+            [0, 1, 2, 0, 0, 1, 1, 2],
             [0, 0, 0, 1, 2, 0, 0, 0],
             [0, 0, 0, 2, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,20 +59,33 @@ def consumoVertical(A:[int], i:int, j:int, turno:int) -> 'void':
             pass
 
 def consumoHorizontal(A:[int], i:int, j:int, turno:int) -> 'void':
-    for j in range(0,8):
-        if ( A[i][j] == turno ):
-            if ( A[i][j+1] != 0 and A[i][j+1] != turno ):
-                A[i][j+1] = turno
-                consumoHorizontal(A, i, j+1, turno)
-            elif ( A[i][j-1] != 0 and A[i][j-1] != turno ):
-                A[i][j-1] == turno
-                consumoHorizontal(A, i, j-1, turno)
-            elif ( A[i][j+1] == 0 or A[i][j+1] == turno ):
-                break
-            elif ( A[i][j-1] == 0 or A[i][j-1] == turno):
-                break
-        else:
-            pass
+    k=int
+    k=j
+    if j!=7:
+        while j<7:
+            if A[i][j+1]!=turno and A[i][j+1]!=0:
+                j=j+1
+            elif A[i][j+1]==0:
+                j=7
+            elif A[i][j+1]==turno:
+                for t in range (k,j+1):
+                    A[i][t]=turno  
+                j=j+1         
+    elif j==7:
+        pass
+    j=k
+    if j!=0:
+        while j>0:
+            if A[i][j-1]!=turno and A[i][j-1]!=0:
+                j=j-1
+            elif A[i][j-1]==0:
+                j=0
+            elif A[i][j-1]==turno:
+                for z in range (j-1,k):
+                    A[i][z]=turno
+                j=j-1
+    elif j==0:
+        pass 
 
 reflejarJugada(Tablero, F, C, Turno)
 consumoVertical(Tablero, F, C, Turno)
--- consumoHorizontal.py
+++ consumoHorizontal.py
@@ -1,15 +1,29 @@
 def consumoHorizontal(A:[int], i:int, j:int, turno:int) -> 'void':
-    for j in range(0,8):
-        if ( A[i][j] == turno ):
-            if ( A[i][j+1] != 0 and A[i][j+1] != turno ):
-                A[i][j+1] = turno
-                consumoHorizontal(A, i, j+1, turno)
-            elif ( A[i][j-1] != 0 and A[i][j-1] != turno ):
-                A[i][j-1] == turno
-                consumoHorizontal(A, i, j-1, turno)
-            elif ( A[i][j+1] == 0 or A[i][j+1] == turno ):
-                break
-            elif ( A[i][j-1] == 0 or A[i][j-1] == turno):
-                break
-        else:
-            break
+    k=int
+    k=j
+    if j!=7:
+        while j<7:
+            if A[i][j+1]!=turno and A[i][j+1]!=0:
+                j=j+1
+            elif A[i][j+1]==0:
+                j=7
+            elif A[i][j+1]==turno:
+                for t in range (k,j+1):
+                    A[i][t]=turno  
+                j=j+1         
+    elif j==7:
+        pass
+    j=k
+    if j!=0:
+        while j>0:
+            if A[i][j-1]!=turno and A[i][j-1]!=0:
+                j=j-1
+            elif A[i][j-1]==0:
+                j=0
+            elif A[i][j-1]==turno:
+                for z in range (j-1,k):
+                    A[i][z]=turno
+                j=j-1
+    elif j==0:
+        pass 
+
