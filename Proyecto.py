390535142f5a6508916b28b3dd590e9e3f471fc0
Gchaurio <17-10126@usb.ve>
2019-11-17 15:22:39 -0400

    Listo consuMOOO ESQUEREEEEEEEEEEEEE

--- Proyecto.py
+++ Proyecto.py
@@ -3,14 +3,14 @@ import sys
 
 
 #Creacion del tablero
-Tablero = [ [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
+Tablero = [ [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 2, 0, 0, 0, 2, 0, 0],
+            [0, 0, 1, 0, 2, 0, 0, 0],
             [0, 0, 0, 1, 2, 0, 0, 0],
-            [0, 0, 2, 2, 1, 0, 0, 0],
-            [0, 2, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0] ]
+            [0, 0, 0, 2, 0, 0, 0, 0],
+            [0, 2, 0, 0, 0, 1, 0, 0],
+            [1, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 2] ]
             
 print(Tablero)
 
@@ -120,7 +120,69 @@ def consumoDiagonal(A:[int], i:int, j:int, turno:int) -> 'void':
                 j = 7
     elif i == 0 and j == 7:
         pass
+    
     i = k
+    j = l
+
+    if i != 7 and j != 7:
+        while i < 7 or j < 7:
+            if A[i+1][j+1] != turno and A[i+1][j+1] != 0:
+                i = i + 1
+                j = j + 1
+            elif A[i+1][j+1] == 0:
+                i = 7
+                j = 7
+            elif A[i+1][j+1] == turno:
+                i = k
+                for r in range(l,j+1):
+                    A[i][r] = turno
+                    i = i + 1
+                i = 7
+                j = 7
+    elif i == 7 and j == 7:
+        pass
+    
+    i=k
+    j=l
+
+    if i != 7 and j != 0:
+        while i < 7 or j > 0:
+            if A[i+1][j-1] != turno and A[i+1][j-1] != 0:
+                i = i + 1
+                j = j - 1
+            elif A[i+1][j-1] == 0:
+                i = 7
+                j = 0
+            elif A[i+1][j-1] == turno:
+                j = l
+                for r in range(k,i+1):
+                    A[r][j] = turno
+                    j = j - 1
+                i = 7
+                j = 0
+    elif i == 7 and j == 0:
+        pass
+    
+    i=k
+    j=l
+
+    if i != 0 and j != 0:
+        while i > 0 or j > 0:
+            if A[i-1][j-1] != turno and A[i-1][j-1] != 0:
+                i = i - 1
+                j = j - 1
+            elif A[i-1][j-1] == 0:
+                i = 0
+                j = 0
+            elif A[i-1][j-1] == turno:
+                for r in range(i-1,k):
+                    A[r][j-1] = turno
+                    j = j + 1
+                i = 0
+                j = 0
+    elif i == 0 and j == 0:
+        pass
+
 
 reflejarJugada(Tablero, F, C, Turno)
 consumoDiagonal(Tablero, F, C, Turno)
--- consumoDiagonal.py
+++ consumoDiagonal.py
@@ -18,4 +18,66 @@ def consumoDiagonal(A:[int], i:int, j:int, turno:int) -> 'void':
                 j = 7
     elif i == 0 and j == 7:
         pass
+    
     i = k
+    j = l
+
+    if i != 7 and j != 7:
+        while i < 7 or j < 7:
+            if A[i+1][j+1] != turno and A[i+1][j+1] != 0:
+                i = i + 1
+                j = j + 1
+            elif A[i+1][j+1] == 0:
+                i = 7
+                j = 7
+            elif A[i+1][j+1] == turno:
+                i = k
+                for r in range(l,j+1):
+                    A[i][r] = turno
+                    i = i + 1
+                i = 7
+                j = 7
+    elif i == 7 and j == 7:
+        pass
+    
+    i=k
+    j=l
+
+    if i != 7 and j != 0:
+        while i < 7 or j > 0:
+            if A[i+1][j-1] != turno and A[i+1][j-1] != 0:
+                i = i + 1
+                j = j - 1
+            elif A[i+1][j-1] == 0:
+                i = 7
+                j = 0
+            elif A[i+1][j-1] == turno:
+                j = l
+                for r in range(k,i+1):
+                    A[r][j] = turno
+                    j = j - 1
+                i = 7
+                j = 0
+    elif i == 7 and j == 0:
+        pass
+    
+    i=k
+    j=l
+
+    if i != 0 and j != 0:
+        while i > 0 or j > 0:
+            if A[i-1][j-1] != turno and A[i-1][j-1] != 0:
+                i = i - 1
+                j = j - 1
+            elif A[i-1][j-1] == 0:
+                i = 0
+                j = 0
+            elif A[i-1][j-1] == turno:
+                for r in range(i-1,k):
+                    A[r][j-1] = turno
+                    j = j + 1
+                i = 0
+                j = 0
+    elif i == 0 and j == 0:
+        pass
+
