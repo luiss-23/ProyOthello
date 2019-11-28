330cfc5e3ee743cd872918cfe4a38c8c27a84bfb
Luis Carlos  Blanco <luiscarlosblanco@MacBook-Pro-de-Luis.local>
2019-11-27 22:28:23 -0400

    No logor que termine cuando ninguno tiene jugadas

--- Proyecto.py
+++ Proyecto.py
@@ -39,31 +39,31 @@ movimientos = 0
 Jugadores = [1,2]
 
 def esValida(A:[int],B:[int],i:int,j:int,turneto:int)->bool:
-    Valida=False
-    if A[i][j]!=0:
+    Valida = False
+    if A[i][j] != 0:
         pass
     elif A[i][j] == 0:
-        for a in range (0,8):
-            for b in range (0,8):
-                B[a][b]=A[a][b]
+        for a in range(0,8):
+            for b in range(0,8):
+                B[a][b] = A[a][b]
         consumo(B, i, j,turneto)
         reflejarJugada(B,i,j,turneto)
-        for a in range (0,8):
+        for a in range(0,8):
             for b in range (0,8):
-                if a==i and b==j:
+                if a == i and b == j:
                     pass
-                elif a!=i or b!=j:
-                    if A[a][b]==B[a][b]:
+                elif a != i or b != j:
+                    if A[a][b] == B[a][b]:
                         pass
-                    elif A[a][b]!=B[a][b]:
-                        Valida=True 
+                    elif A[a][b] != B[a][b]:
+                        Valida = True 
     return Valida
 
 def cambiarJugador(turn:int) -> 'void':
-    if turn==1:
-        turn=2
-    elif turn==2:
-        turn=1
+    if turn == 1:
+        turn = 2
+    elif turn == 2:
+        turn = 1
     return turn
 
 
@@ -71,61 +71,61 @@ def reflejarJugada( A: [int], i: int, j: int, turno: int) -> 'void':
     A[i][j] = turno
 
 def consumoVertical(A:[int], i:int, j:int, turno:int) -> 'void':
-    k=int
-    k=i
-    if i!=7:
-        while i<7:
-            if A[i+1][j]!=turno and A[i+1][j]!=0:
-                i=i+1
-            elif A[i+1][j]==0:
-                i=7
-            elif A[i+1][j]==turno:
-                for t in range (k,i+1):
-                    A[t][j]=turno  
+    k = int
+    k = i
+    if i != 7:
+        while i < 7:
+            if A[i+1][j] != turno and A[i+1][j] != 0:
+                i = i + 1
+            elif A[i+1][j] == 0:
+                i = 7
+            elif A[i+1][j] == turno:
+                for t in range(k,i+1):
+                    A[t][j] = turno  
                 i=7        
-    elif i==7:
+    elif i == 7:
         pass
-    i=k
-    if i!=0:
-        while i>0:
-            if A[i-1][j]!=turno and A[i-1][j]!=0:
-                i=i-1
-            elif A[i-1][j]==0:
+    i = k
+    if i != 0:
+        while i > 0:
+            if A[i-1][j] != turno and A[i-1][j] != 0:
+                i = i - 1
+            elif A[i-1][j] == 0:
+                i = 0
+            elif A[i-1][j] == turno:
+                for z in range(i-1,k):
+                    A[z][j] = turno
                 i=0
-            elif A[i-1][j]==turno:
-                for z in range (i-1,k):
-                    A[z][j]=turno
-                i=0
-    elif i==0:
+    elif i == 0:
         pass 
 
 def consumoHorizontal(A:[int], i:int, j:int, turno:int) -> 'void':
-    k=int
-    k=j
-    if j!=7:
-        while j<7:
-            if A[i][j+1]!=turno and A[i][j+1]!=0:
-                j=j+1
-            elif A[i][j+1]==0:
-                j=7
-            elif A[i][j+1]==turno:
-                for t in range (k,j+1):
-                    A[i][t]=turno  
-                j=7        
-    elif j==7:
+    k = int
+    k = j
+    if j != 7:
+        while j < 7:
+            if A[i][j+1] != turno and A[i][j+1] != 0:
+                j = j + 1
+            elif A[i][j+1] == 0:
+                j = 7
+            elif A[i][j+1] == turno:
+                for t in range(k,j+1):
+                    A[i][t] = turno  
+                j = 7        
+    elif j == 7:
         pass
-    j=k
-    if j!=0:
-        while j>0:
-            if A[i][j-1]!=turno and A[i][j-1]!=0:
-                j=j-1
-            elif A[i][j-1]==0:
-                j=0
-            elif A[i][j-1]==turno:
-                for z in range (j-1,k):
-                    A[i][z]=turno
-                j=0
-    elif j==0:
+    j = k
+    if j != 0:
+        while j > 0:
+            if A[i][j-1] != turno and A[i][j-1] != 0:
+                j = j - 1
+            elif A[i][j-1] == 0:
+                j = 0
+            elif A[i][j-1] == turno:
+                for z in range(j-1,k):
+                    A[i][z] = turno
+                j = 0
+    elif j == 0:
         pass 
 
 def consumoDiagonal(A:[int], i:int, j:int, turno:int) -> 'void':
@@ -153,7 +153,7 @@ def consumoDiagonal(A:[int], i:int, j:int, turno:int) -> 'void':
     j = l
 
     if i != 7 and j != 7:
-        while i <7 and j <7:
+        while i < 7 and j < 7:
             if A[i+1][j+1] != turno and A[i+1][j+1] != 0:
                 i = i + 1
                 j = j + 1
@@ -212,10 +212,10 @@ def consumoDiagonal(A:[int], i:int, j:int, turno:int) -> 'void':
         pass
 
 def quedanFichas(F:int,R:int)->bool:
-    if F-R==0:
-        quedan=False
-    elif F-R!=0:
-        quedan=True
+    if F - R == 0:
+        quedan = False
+    elif F - R != 0:
+        quedan = True
     return quedan
 
 def consumo(A:[int], i:int, j:int, turno:int) -> 'void':
@@ -224,23 +224,23 @@ def consumo(A:[int], i:int, j:int, turno:int) -> 'void':
     consumoVertical(A, i, j, Turno)
 
 def QuedanJugadas(A:[int],B:[int],turno:int)->bool:
-    Quedan=False
-    for x in range(0,8):
-        for y in range (0,8):
-            if A[x][y]==0:
-                if esValida(A,B,x,y,turno):
-                    Quedan=True
+    Quedan = False
+    for i in range(0,8):
+        for j in range (0,8):
+            if A[i][j] == 0:
+                if esValida(A,B,i,j,turno):
+                    Quedan = True
                 else:
                     pass
-            elif A[x][y]==1 or A[x][y]==2:
+            elif A[i][j] == 1 or A[i][j] == 2:
                 pass
     return Quedan
 
 Turno = random.choice(Jugadores)
 print('El primer jugador es: ' +str(Turno))
 
-while quedanFichas(fichas, movimientos) == True and (QuedanJugadas(Tablero,Copia,1)==True or QuedanJugadas(Tablero,Copia,2)==True):
-    if QuedanJugadas(Tablero,Copia,Turno):
+while quedanFichas(fichas, movimientos):
+    if  QuedanJugadas(Tablero, Copia, Turno) == True :
         F = int(input('Introduza la fila donde se hara el movimiento: '))
         C = int(input('Introduza la columna donde se hara el movimiento: '))
         if esValida(Tablero,Copia,F,C,Turno):
@@ -256,5 +256,6 @@ while quedanFichas(fichas, movimientos) == True and (QuedanJugadas(Tablero,Copia
         else:
             print("La jugada introducida no es valida.")
             print("Para que una jugada sea valida, la casilla no debe estar ocupada por una ficha y se deben consumir almenos una ficha.")
-    else: Turno = cambiarJugador(Turno)
-print("Se termina el juego porque no se pueden colocar mas fichas")
+    else:
+        Turno = cambiarJugador(Turno)
+        print('El turno es para el jugador: ' +str(Turno))
